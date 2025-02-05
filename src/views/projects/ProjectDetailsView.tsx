7dec73be77d1f8f360dfb5f91ae72a75e0962349
Joedash <joedash77@gmail.com>
2025-02-05 16:50:09 -0300

    cambios

--- src/views/projects/ProjectDetailsView.tsx
+++ src/views/projects/ProjectDetailsView.tsx
@@ -37,9 +37,8 @@ export default function ProjectDetailsView() {
                     >Agregar Tarea</button>
 
                     <Link
-                        to={'/projects/:projectId/team'}
+                        to={'team'}
                         className="bg-fuchsia-600 hover:bg-fuchsia-700 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors"
-
                     >Colaboradores
                     </Link>
                 </nav>
