3087d89d74e1c62a42af60da82b17ce43e1966bb
Joedash <joedash77@gmail.com>
2025-02-05 22:02:17 -0300

    errores

--- src/components/tasks/TaskList.tsx
+++ src/components/tasks/TaskList.tsx
@@ -94,8 +94,8 @@ export default function TaskList({ tasks, canEdit }: TaskListProps) {
                     <div key={status} className='min-w-[300px] 2xl:min-w-0 2xl:w-1/5'>
                         <h3 
                             className={`capitalize text-xl font-light border border-slate-300
-                            bg-white p-3 border-t-8 ${statusStyles[status]}`}
-                        >{statusTranslations[status]}</h3>
+                            bg-white p-3 border-t-8`}
+                        ></h3>
 
                         <DropTask status={status}/>
 
