7fcd3716e0af64998bbee8d9a29802c8278feebf
Joedash <joedash77@gmail.com>
2025-02-05 22:04:47 -0300

    cacas

--- src/components/tasks/TaskList.tsx
+++ src/components/tasks/TaskList.tsx
@@ -92,11 +92,6 @@ export default function TaskList({ tasks, canEdit }: TaskListProps) {
                 <DndContext onDragEnd={handleDragEnd}>  
                 {Object.entries(groupedTasks).map(([status, tasks]) => (
                     <div key={status} className='min-w-[300px] 2xl:min-w-0 2xl:w-1/5'>
-                        <h3 
-                            className={`capitalize text-xl font-light border border-slate-300
-                            bg-white p-3 border-t-8`}
-                        >{statusTranslations[status]}</h3>
-
                         <DropTask status={status}/>
 
                         <ul className='mt-5 space-y-5'>
